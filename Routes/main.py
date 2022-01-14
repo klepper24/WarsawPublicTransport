43e8a21e53e1d6c2b7bc9a70af09a63cb80cd363
mklep <m.j.klepacki@gmail.com>
2022-01-14 11:41:13 +0100

    json refactoring and uploading data to redis

--- Routes/main.py
+++ Routes/main.py
@@ -1,35 +1,45 @@
-import json
 import time
 from collections import deque
+import redis
+import json
 
 
-def cut_file():
-    with open("RA211227.txt", "rt", encoding="ansi") as file:
-        f = open("result.txt", "w", encoding='utf-8')
-        previous_line = ""
-        for line in file:
-            if ('*TR') in line or ('*LW') in line:
-                try:
-                    f.write(previous_line)
-                    print(previous_line)
-                except StopIteration:
-                    # there is no lines left
-                    break
+def send_json_to_redis(key, value):
+    r = redis.StrictRedis()
+    r.execute_command('JSON.SET', key, '.', json.dumps(value))
 
-                while ('#LW') not in line:
-                    try:
-                        f.write(line)
-                        print(line)
-                        line = next(file)
-                    except StopIteration:
-                        # there is no lines left
-                        break
-            previous_line = line
-        f.close()
+
+def get_json_from_redis(key, path='.'):
+    r = redis.StrictRedis()
+    return json.loads(r.execute_command('JSON.GET', key, path))
+
+
+# def cut_file():
+#     with open("RA211227.txt", "rt", encoding="ansi") as file:
+#         f = open("result.txt", "w", encoding='utf-8')
+#         previous_line = ""
+#         for line in file:
+#             if ('*TR') in line or ('*LW') in line:
+#                 try:
+#                     f.write(previous_line)
+#                     print(previous_line)
+#                 except StopIteration:
+#                     # there is no lines left
+#                     break
+#
+#                 while ('#LW') not in line:
+#                     try:
+#                         f.write(line)
+#                         print(line)
+#                         line = next(file)
+#                     except StopIteration:
+#                         # there is no lines left
+#                         break
+#             previous_line = line
+#         f.close()
 
 
 def create_json():
-    routes_json = []
     with open("result.txt", "rt", encoding="utf-8") as file:
         for line in file:
             if 'Linia' in line and 'TRAMWAJOWA' in line:
@@ -42,11 +52,12 @@ def create_json():
                 routes = []
                 for _ in range(number_of_routes):
                     line = next(file)
-                    route_nr = line.split()[0]
+                    route_name = line.split()[0]
                     line = next(file)
                     number_of_stops = int(line.split()[1])
 
                     street = ""
+                    stop_number = 1
                     stops = []
                     for _ in range(number_of_stops):
                         r_flag = False
@@ -57,19 +68,19 @@ def create_json():
                             r_flag = True
                             data.remove('r')
                         if data[0].isdigit():
-                            stop_number = int(data[0])
+                            stop_unit_post = int(data[0])
                         else:
                             street = ""
                             for element in data:
                                 if element.isdigit():
-                                    stop_number = int(element)
+                                    stop_unit_post = int(element)
                                     break
                                 else:
                                     street += "".join(element)
                                     street += " "
                         data_string = " ".join(data)
                         data_string = data_string.replace(street, "", 1)
-                        data_string = data_string.replace(str(stop_number), "")
+                        data_string = data_string.replace(str(stop_unit_post), "")
                         data_string = data_string.strip()
 
                         stop_name = data_string.split(',')[0]
@@ -82,7 +93,8 @@ def create_json():
                             data_string.split('|')[2].lstrip())
 
                         stop_json = {
-                            'stop_nr': stop_number,
+                            'stop_number': stop_number,
+                            'stop_unit_post': stop_unit_post,
                             'stop_name': stop_name,
                             'street': street,
                             'r_flag': r_flag,
@@ -90,9 +102,10 @@ def create_json():
                             'max_time': max_time
                         }
                         stops.append(stop_json)
+                        stop_number += 1
 
                     route = {
-                        'route_nr': route_nr,
+                        'route_name': route_name,
                         'number_of_stops': number_of_stops,
                         "stops": stops
                     }
@@ -103,20 +116,15 @@ def create_json():
                     'number_of_routes': number_of_routes,
                     'routes': routes
                 }
-                routes_json.append(line)
-
-    with open('routes_json.json', 'w', encoding='utf-8') as outfile:
-        json.dump(routes_json, outfile, indent=4, ensure_ascii=False )
-
+                send_json_to_redis(line_nr, line)
 
 
 def main():
     # cut_file()
-    create_json()
+    # create_json()
+    reply = get_json_from_redis('3', '.routes[1].stops[0]')
+    print(reply)
 
 
 if __name__ == "__main__":
     main()
-
-
-
