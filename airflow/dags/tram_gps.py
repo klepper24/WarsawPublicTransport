274c28d226cd3bf570b2f036e59f594bbc98e3f3
mklep <m.j.klepacki@gmail.com>
2022-06-07 10:16:02 +0200

    saving Tram GPS with Airflow

--- airflow/dags/tram_gps.py
+++ airflow/dags/tram_gps.py
@@ -2,9 +2,12 @@ import json
 from bson.json_util import dumps
 from datetime import timedelta
 from datetime import datetime
+from dateutil import tz
+import requests
+import logging
+import os
 
-import airflow
-from airflow import DAG
+from airflow.decorators import dag, task
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.operators.http_operator import SimpleHttpOperator
 from airflow.operators.python_operator import PythonOperator
@@ -16,7 +19,7 @@ from airflow.providers.mongo.hooks.mongo import MongoHook
 default_args = {
     'owner': 'Michal Klepacki',
     'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(0),
+    'start_date': datetime(2022, 5, 1),
     'email': ['mklep@softserveinc.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -26,56 +29,46 @@ default_args = {
 api_key = Variable.get("api_key")
 resource_id = 'f2e5503e927d-4ad3-9500-4ab9e55deb59'
 
-def save_tram_gps(ti) -> None:
-    trams = ti.xcom_pull(task_ids=['get_trams_gps'])
-    with open('logs/tram_test.json', 'w') as f:
-        json.dump(trams[0], f)
-        
-def test_mongo():
-    meta_base = MongoHook('my_mongo')
-
-    doc = meta_base.get_collection('Stops', 'WarsawPublicTransport')
-    with open('logs/mongo_test.json', 'w') as file:
-        file.write('[')
-        for document in doc.find():
-            file.write(dumps(document))
-            file.write(',')
-        file.write(']')
-
  
-with DAG(
+@dag(
 	dag_id='ztm_gps_collector', 
-	description='Collects busses and trams GPS',
-    schedule_interval='@daily', 
+	description='Collects trams GPS',
+    schedule_interval='*/1 * * * *', 
 	start_date=datetime(2022, 5, 1),
     default_args=default_args,
-    catchup=True
-) as dag:
+    catchup=False
+) 
+def saveTramLocation():
 
     task_is_api_active = HttpSensor(
         task_id='is_api_active',
         http_conn_id='url_ztm_gps',
         endpoint='api/action/busestrams_get/',
-        request_params={"type": "2", 'resource_id': resource_id, 'apikey' : api_key}
+        request_params={'type': '2', 'resource_id': resource_id, 'apikey' : api_key}
     )
 
-    task_get_trams = SimpleHttpOperator(
-        task_id='get_trams_gps',
-        http_conn_id='url_ztm_gps',
-        endpoint='api/action/busestrams_get/',
-        method='GET',
-        data={"type": "2", 'resource_id': resource_id, 'apikey' : api_key}
-	)
-    
-    task_save = PythonOperator(
-        task_id='save_tram_gps',
-        python_callable=save_tram_gps
-    )
-    
-    my_mongo_test = PythonOperator(
-        task_id='my_mongo_test',
-        python_callable=test_mongo
-    )
-	 
+    @task(task_id='save_tram_gps', retries=2)
+    def save_tram_gps():
+        to_zone = tz.gettz('Europe/Warsaw')
+        today_time = datetime.now().astimezone(to_zone)
+        
+        date = today_time.strftime("%Y%m%d")
+        time = today_time.strftime("%H%M%S")
+        url = 'https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=f2e5503e927d-4ad3-9500-4ab9e55deb59&type=2&apikey='+api_key
+        r = requests.get(url)
+        status = r.status_code
+        logging.info(f'Status: {status}')
+        if status == 200:
+            json_response = r.json()
 
-task_is_api_active >> task_get_trams >> task_save >> my_mongo_test
+            # saving json to file
+            filename = f'dags/tram_data/{date}/tram{time}.json'
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(json_response, f, ensure_ascii=False, indent=4)
+    
+ 
+
+    task_is_api_active >> save_tram_gps()
+
+dag = saveTramLocation()
