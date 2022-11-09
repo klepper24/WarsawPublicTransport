687bc13db5caeaed2190cc094aa8f0ce29539b03
Mikolaj Giermek <mgierm@gmail.com>
2022-11-09 11:05:36 +0100

    Common variables extracted to settings.py, added method to generate API Warszawa url

---
+++ orchestrator/dags/settings.py
@@ -0,0 +1,17 @@
+import os
+from datetime import datetime
+
+from dateutil.tz import tz
+
+
+SPARK_MASTER = "spark://spark:7077"
+SPARK_OUT_DIR = '/opt/spark/execution_scripts'
+
+NOW = datetime.now()
+TODAY_DATETIME = datetime.now().astimezone(tz.gettz('Europe/Warsaw'))
+TODAY_DATE = TODAY_DATETIME.strftime("%Y%m%d")
+TODAY_TIME = TODAY_DATETIME.strftime("%H%M%S")
+
+TRAM_FOLDER = os.path.join(SPARK_OUT_DIR, TODAY_DATE)
+
+API_WARSZAWA_URL = 'https://api.um.warszawa.pl/api/action/busestrams_get/'
---
+++ orchestrator/dags/utils.py
@@ -0,0 +1,12 @@
+from orchestrator.dags import settings
+
+
+def generate_api_warszawa_url(api_key: str, resource_id: str, **kwargs):
+    """
+    Returns url to Warsaw Public Transport API with given parameters as query strings
+    """
+    url = settings.API_WARSZAWA_URL
+    url += f'?apikey={api_key}&resource_id={resource_id}'
+    for k, v in kwargs.items():
+        url += f'&{k}={v}'
+    return url
