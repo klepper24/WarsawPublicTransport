4a75cec185d5d29768128d318d4139564531fb06
mklep <m.j.klepacki@gmail.com>
2023-02-11 12:17:45 +0100

    some additional changes to postgres ddl

--- postgresdb/postgres_ddl.sql
+++ postgresdb/postgres_ddl.sql
@@ -1,64 +1,71 @@
 CREATE TABLE "stops" (
-  "unit_post" int PRIMARY KEY,
-  "full_name" int,
-  "stop_longitude" double,
-  "stop_latitude" double,
-  "street" varchar,
-  "unit" varchar,
-  "post" varchar,
-  "is_depot" int,
-  "created_at" datetime DEFAULT (now())
+  "id" serial PRIMARY KEY,
+  "full_name" varchar(100),
+  "stop_longitude" double precision,
+  "stop_latitude" double precision,
+  "street" varchar(80),
+  "unit" varchar(4),
+  "post" varchar(2),
+  "is_depot" boolean,
+  "created_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "tram_states" (
-  "id" int PRIMARY KEY,
+  "id" bigserial PRIMARY KEY,
   "current_tram_time" timestamp,
-  "stop_state" varchar,
-  "tram_id" int,
-  "route_variant_id" int,
-  "tram_longitude" double,
-  "tram_latitude" double,
-  "distance" int
+  "stop_state" varchar(20),
+  "tram_id" integer,
+  "route_variant_id" integer,
+  "tram_longitude" double precision,
+  "tram_latitude" double precision,
+  "distance" integer
 );
 
 CREATE TABLE "routes" (
-  "id" int PRIMARY KEY,
-  "name" varchar UNIQUE,
-  "stops_cnt" int,
-  "created_at" datetime DEFAULT (now())
+  "id" serial PRIMARY KEY,
+  "name" varchar(30) UNIQUE,
+  "stops_cnt" smallint,
+  "created_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "route_variants" (
-  "id" int PRIMARY KEY,
-  "stop_id" int NOT NULL,
-  "route_id" int NOT NULL,
-  "stop_sequence_nr" int
+  "id" serial PRIMARY KEY,
+  "stop_id" integer NOT NULL,
+  "route_id" integer NOT NULL,
+  "stop_sequence_nr" smallint
 );
 
 CREATE TABLE "timetables" (
-  "id" int PRIMARY KEY,
+  "id" serial PRIMARY KEY,
   "day_type" varchar,
-  "departure_time" datetime,
-  "departure_time_sequence_nr" int,
-  "stop_id" int NOT NULL,
-  "line_nr" varchar,
-  "created_at" datetime DEFAULT (now())
+  "departure_time" time,
+  "departure_time_sequence_nr" smallint,
+  "stop_id" integer NOT NULL,
+  "line_nr" varchar(3),
+  "created_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "trams" (
-  "id" int PRIMARY KEY,
-  "vehicle_nr" varchar,
-  "brigade" varchar,
-  "line_nr" varchar,
-  "created_at" datetime DEFAULT (now())
+  "id" serial PRIMARY KEY,
+  "vehicle_nr" varchar(10),
+  "brigade" varchar(4),
+  "line_nr" varchar(3),
+  "created_at" timestamp DEFAULT (now())
 );
 
 ALTER TABLE "tram_states" ADD FOREIGN KEY ("tram_id") REFERENCES "trams" ("id");
 
 ALTER TABLE "tram_states" ADD FOREIGN KEY ("route_variant_id") REFERENCES "route_variants" ("id");
 
-ALTER TABLE "route_variants" ADD FOREIGN KEY ("stop_id") REFERENCES "stops" ("unit_post");
+ALTER TABLE "route_variants" ADD FOREIGN KEY ("stop_id") REFERENCES "stops" ("id");
 
 ALTER TABLE "route_variants" ADD FOREIGN KEY ("route_id") REFERENCES "routes" ("id");
 
-ALTER TABLE "timetables" ADD FOREIGN KEY ("stop_id") REFERENCES "stops" ("unit_post");
+ALTER TABLE "timetables" ADD FOREIGN KEY ("stop_id") REFERENCES "stops" ("id");
+
+ALTER TABLE "tram_states" ADD CONSTRAINT "possible_tram_states"
+  CHECK ("stop_state" IN (
+	'non-visited'  -- tram is before the next stop
+    'occupied', -- tram is within 20m diameter to stop
+    'visited' -- tram with occupied state leaves 20m diameter
+  ));
