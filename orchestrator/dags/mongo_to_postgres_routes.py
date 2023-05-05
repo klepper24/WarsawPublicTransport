fb41a3dcbf2ae53285ada0d9940f3cc26e09f25d
mklep <m.j.klepacki@gmail.com>
2023-05-05 10:25:02 +0200

    results pagination

--- orchestrator/dags/mongo_to_postgres_routes.py
+++ orchestrator/dags/mongo_to_postgres_routes.py
@@ -1,4 +1,3 @@
-from itertools import tee
 from datetime import datetime
 from typing import Collection
 
@@ -30,51 +29,52 @@ def create_postgres_session(postgres_url: str):
     return sessionmaker(source_engine)
 
 
-def map_routes_to_postgres_object(routes: object) -> object:
-    id_nr = 1
-    for doc in routes:
+def map_routes_to_postgres_object(routes: object, enum_start: int) -> object:
+    for id_nr, doc in enumerate(routes, start=1+enum_start):
         route = Routes(id=id_nr, line_name=doc["line_nr"] + doc["routes"]["route_nr"],
                        line_nr=doc["line_nr"], name=doc["routes"]["route_nr"],
                        stops_cnt=doc["routes"]["number_of_stops"],
                        created_at=datetime.now())
-        id_nr += 1
         yield route
 
 
 def send_routes_to_postgres() -> None:
-    routes_collection = get_mongo_collection('Routes')
-    pipeline = [
-        {"$unset": ["_id", "number_of_routes", "routes.stops"]},
-        {"$unwind": "$routes"}
-    ]
-    agg_routes = routes_collection.aggregate(pipeline)
-
-    mapped_routes = map_routes_to_postgres_object(agg_routes)
-    mapped_routes, mapped_routes_backup = tee(mapped_routes)
-    generated_mapped_routes = (row for row in mapped_routes)
-
     postgres_session = create_postgres_session(settings.POSTGRES_URL)
-
     with postgres_session() as session:
         session.execute(text('TRUNCATE TABLE dbo.routes CASCADE'))
         session.commit()
-        try:
-            session.bulk_save_objects(generated_mapped_routes)
-            session.commit()
-        except IntegrityError:
-            print("1st try: An IntegrityError has occurred")
-            # do not commit this row
-            session.rollback()
-            for route in mapped_routes_backup:
-                session.add(route)
-                try:
-                    session.commit()
-                except IntegrityError:
-                    print("2nd try: An IntegrityError has occurred")
-                    # do not commit this row
-                    session.rollback()
+    limit = 10
+    offset = 0
+    routes_collection = get_mongo_collection('Routes')
+    while True:
+        pipeline = [
+            {"$unset": ["_id", "number_of_routes", "routes.stops"]},
+            {"$unwind": "$routes"},
+            {"$skip": offset},
+            {"$limit": limit}
+        ]
+        agg_routes = routes_collection.aggregate(pipeline)
+        mapped_routes = list(map_routes_to_postgres_object(agg_routes, offset))
+        with postgres_session() as session:
+            try:
+                session.bulk_save_objects(mapped_routes)
+                session.commit()
+            except IntegrityError:
+                print("1st try: An IntegrityError has occurred")
+                # do not commit this row
+                session.rollback()
+                for route in mapped_routes:
+                    session.add(route)
+                    try:
+                        session.commit()
+                    except IntegrityError:
+                        print("2nd try: An IntegrityError has occurred")
+                        # do not commit this row
+                        session.rollback()
+        if len(mapped_routes) < limit:
+            break
+        offset = offset + limit
 
 
 if __name__ == "__main__":
     send_routes_to_postgres()
-    
