f124baab5bfffc702c3a3a0bea5b7b55f3aa0bd9
Michal Klepacki <mklep@softserveinc.com>
2023-07-19 12:07:36 +0200

    Revert "Dodanie pliku json do trams"
    This reverts commit c8ace88e1b5532af7c8dd83b6a040bcb8767307d.

--- orchestrator/dags/settings.py
+++ orchestrator/dags/settings.py
@@ -16,7 +16,7 @@ TODAY_TIME = TODAY_DATETIME.strftime("%H%M%S")
 
 DELAYS_PROCESSING_HOUR = 23
 
-TRAM_FOLDER = f'{SPARK_OUT_DIR}trams_data/'
+TRAM_FOLDER = f'{SPARK_OUT_DIR}{TODAY_DATE}/'
 
 ZTM_GENERAL_LINK = 'ftp://rozklady.ztm.waw.pl'
 
